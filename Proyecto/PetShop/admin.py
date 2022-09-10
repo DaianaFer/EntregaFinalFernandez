3ae5538a59ead1ec6b2b3f224642bed0b6ac09f9
Daiana Fernandez <daianadfernandez@gmail.com>
2022-09-10 10:13:28 -0300

    master

--- Proyecto/PetShop/admin.py
+++ Proyecto/PetShop/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from PetShop.models import *
+from Auten.models import *
 
 # Register your models here.
 admin.site.register(Productos)
 admin.site.register(Contacto)
 admin.site.register(Servicios)
+admin.site.register(Avatar)
